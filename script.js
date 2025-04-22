b42e3e69056dbd14767d1e37f5c67a8a7123fcde
korobkin <nikita.korobkin.personal@gmail.com>
2025-04-22 16:45:20 +0300

    Rework for event listeners

--- script.js
+++ script.js
@@ -1,70 +1,104 @@
+// "Global" functions
+
 function capitalize(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
 
-function getComputerChoice() {
-    const randomNum = Math.random();
-    if (randomNum < 1 / 3) {
-        return ("rock");
-    } else if (randomNum < 2 / 3) {
-        return ("paper");
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// Variables
+
+const playOptions = ["rock", "paper", "scissors"];
+const [player1, player2] = ["human", "computer"];
+let [score1, score2] = [0, 0];
+const maxRounds = 5;
+
+// "Local" helper functions
+
+function getRandomPlay() {
+    return getRandomElement(playOptions);
+}
+
+function getCounterplay(play = getRandomPlay()) {
+    return playOptions.filter((e) => e != play)[0];
+}
+
+function getWinner(play1, play2) {
+    if (getCounterplay(play1) == play2) {
+        return player2;
+    } else if (play1 == play2) {
+        return null;
     } else {
-        return ("scissors");
+        return player1;
     }
 }
 
-function getHumanChoice() {
-    return prompt("What's your move?", "rock");
+function printCurrentScore() {
+    return `${score1} — ${score2}`;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function updateScoreboard(msg) {
+    const para = document.createElement("p");
+    para.textContent = msg;
 
-    function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
-        humanChoice = humanChoice.toLowerCase();
-        let winner;
-
-        if (humanChoice === "rock") {
-            if (computerChoice === "paper") {
-                winner = "computer";
-            } else if (computerChoice === "scissors") {
-                winner = "human";
-            }
-        } else if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                winner = "human";
-            } else if (computerChoice === "scissors") {
-                winner = "computer";
-            }
-        } else if (humanChoice === "scissors") {
-            if (computerChoice === "rock") {
-                winner = "computer";
-            } else if (computerChoice === "paper") {
-                winner = "human";
-            }
-        }
-
-        if (winner === "human") {
-            humanScore += 1;
-            console.log(`You win! ${capitalize(humanChoice)} beats ${computerChoice}.`);
-        } else if (winner === "computer") {
-            computerScore += 1;
-            console.log(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}.`);
-        } else {
-            console.log(`Draw! Both played ${humanChoice}.`);
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-
-    const winner = humanScore > computerScore ? "human" : "computer";
-
-    console.log(
-        `Final score: ${humanScore} - ${computerScore}. ${capitalize(winner)} wins!`
-    )
+    scoreboard.appendChild(para);
 }
 
-playGame();
+function isLastRound() {
+    return [score1, score2].some((s) => s == maxRounds);
+}
+
+// Event handlers
+
+function handlePlay(e) {
+    playRound(e.target.value);
+}
+
+// Rendering the picker and the scoreboard
+
+const playPicker = document.querySelector("#play-options");
+const scoreboard = document.querySelector("#scoreboard");
+
+
+for (const play of playOptions) {
+    const btn = document.createElement("button");
+    btn.value = play;
+    btn.textContent = capitalize(play);
+    btn.addEventListener("click", handlePlay);
+
+    playPicker.appendChild(btn);
+}
+
+// Game logic
+
+function playRound(play1, play2 = getRandomPlay()) {
+    let winner = getWinner(play1, play2);
+    let message;
+    
+    if (winner == player2) {
+        score2++;
+        message =
+        `You lose! ${capitalize(play2)} beats ${play1}.`;
+    } else if (winner === null) {
+        message = `Draw! Both played ${play1}.`;
+    } else {
+        score1++;
+        message =
+        `You win! ${capitalize(play1)} beats ${play2}.`;
+    }
+    message += ` Current score: ${printCurrentScore()}`;
+    updateScoreboard(message);
+    
+    if (isLastRound()) {
+        const btns = document.querySelectorAll("button");
+        btns.forEach((btn) => {
+            btn.removeEventListener("click", handlePlay);
+        });
+        
+        message =
+        `Final score: ${printCurrentScore()}. ${capitalize(winner)} wins!`;
+        updateScoreboard(message);
+    }
+}
